b5c52149a010c0fcd86043795694c781881c6c78
Nick Nestle <nick.nestle@gmail.com>
2025-04-21 08:47:50 -0700

    With examples

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import UserForm from "./pages/UserForm";
 import FigmaImports from "./components/figma-imports";
+import Catalog from "./pages/Catalog";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<UserForm />} />
       <Route path="/figma-imports" element={<FigmaImports />} />
+      <Route path="/catalog" element={<Catalog />} />
     </Routes>
   );
 }
---
+++ src/pages/Catalog.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import CatalogBody from "../components/CatalogBody";
+
+const Catalog: React.FC = () => {
+  return (
+    <div className="catalog-container">
+      <CatalogBody />
+    </div>
+  );
+};
+
+export default Catalog;
